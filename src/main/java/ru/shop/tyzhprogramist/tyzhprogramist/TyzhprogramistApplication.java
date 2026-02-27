0803c5686a64fa8c56267fa0c8c56eb3346a2fee
dinya1234 <ulianagaladzeva@gmail.com>
2026-02-27 11:38:23 +0300

    create

---
+++ src/main/java/ru/shop/tyzhprogramist/tyzhprogramist/TyzhprogramistApplication.java
@@ -0,0 +1,13 @@
+package ru.shop.tyzhprogramist.tyzhprogramist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyzhprogramistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TyzhprogramistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/shop/tyzhprogramist/tyzhprogramist/TyzhprogramistApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shop.tyzhprogramist.tyzhprogramist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyzhprogramistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
