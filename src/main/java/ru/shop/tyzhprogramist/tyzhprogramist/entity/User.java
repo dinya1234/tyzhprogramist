80c13512dd798b3f582476564dc3f0661e6a3fc7
dinya1234 <ulianagaladzeva@gmail.com>
2026-02-27 21:57:23 +0300

    энтити юзер

---
+++ src/main/java/ru/shop/tyzhprogramist/tyzhprogramist/entity/User.java
@@ -0,0 +1,101 @@
+package ru.shop.tyzhprogramist.tyzhprogramist.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "first_name", nullable = false)
+    private String first_name;
+
+    @Column(name = "last_name", nullable = false)
+    private String last_name;
+
+    @Column(name = "date_joined", nullable = false)
+    private LocalDateTime date_joined;
+
+
+    public User(){}
+
+    public User (String username, String email , String password , String first_name , String last_name) {
+    this.date_joined= LocalDateTime.now();
+    this.email= email;
+    this.first_name= first_name;
+    this.username=username;
+    this.last_name=last_name;
+    this.password=password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getDate_joined() {
+        return date_joined;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDate_joined(LocalDateTime date_joined) {
+        this.date_joined = date_joined;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
