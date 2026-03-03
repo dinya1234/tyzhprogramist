7ede45ae8123e402a3f9366d2599c3345e1a528a
dinya1234 <ulianagaladzeva@gmail.com>
2026-03-03 14:34:09 +0300

    Создан класс ProductItem и enum ParentType

---
+++ src/main/java/ru/shop/tyzhprogramist/tyzhprogramist/entity/ParentType.java
@@ -0,0 +1,7 @@
+package ru.shop.tyzhprogramist.tyzhprogramist.entity;
+
+public enum ParentType {
+    CART,      // корзина
+    ORDER,     // заказ
+    PC_BUILD   // сборка ПК
+}
---
+++ src/main/java/ru/shop/tyzhprogramist/tyzhprogramist/entity/ProductItem.java
@@ -0,0 +1,43 @@
+package ru.shop.tyzhprogramist.tyzhprogramist.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "product_items")
+public class ProductItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)//доделать/переделать пока хз как реализовывать мб так и оставим
+    @Column(name = "parent_type", nullable = false)
+    private ParentType parentType;
+
+    @Column(name = "parent_id", nullable = false)
+    private Long parentId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity = 1;
+
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    public ProductItem() {}
+
+    public ProductItem( Long parentId, Product product, Integer quantity, BigDecimal price) {
+        this.parentId = parentId;
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
