5e6acce4361209bd2d5a3e2f094c63e888eeae98
Voldemar18 <voldemar.user18@yandex.ru>
2026-03-03 12:54:08 +0300

    Добавление затычки для репозитория продукта

--- src/main/java/ru/shop/tyzhprogramist/tyzhprogramist/repository/ProductRepository.java
+++ src/main/java/ru/shop/tyzhprogramist/tyzhprogramist/repository/ProductRepository.java
@@ -1,5 +1,14 @@
 package ru.shop.tyzhprogramist.tyzhprogramist.repository;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.shop.tyzhprogramist.tyzhprogramist.entity.Product;
+import ru.shop.tyzhprogramist.tyzhprogramist.entity.User;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<User> findByProductname(String username);
+
 
 }
