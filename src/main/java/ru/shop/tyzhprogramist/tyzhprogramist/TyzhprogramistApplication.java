d534fcf99ba0850415cd376860b73003e4a31c57
dinya1234 <ulianagaladzeva@gmail.com>
2026-04-28 15:41:57 +0300

    пытаемся починить гитхаб

--- src/main/java/ru/shop/tyzhprogramist/tyzhprogramist/TyzhprogramistApplication.java
+++ src/main/java/ru/shop/tyzhprogramist/tyzhprogramist/TyzhprogramistApplication.java
@@ -10,7 +10,7 @@ import java.io.InputStream;
 
 @SpringBootApplication
 public class TyzhprogramistApplication {
-
+//проверка для гита
     private static Process reactProcess;
 
     public static void main(String[] args) {
